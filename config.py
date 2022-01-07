ec4c288bc7c1ffe8a9961c91b7042d2bc1d54ae0
Jacob <61052022+Airiuwu@users.noreply.github.com>
2022-01-07 10:47:18 -0800

    ...

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 settings = {
-    'token': 'NTI1NDE3NDU4MjQ5NzYwNzcx.XBwDIw.YGCQf3qlCZNlA674wNUjQr_DGFE',
-    'bot': 'weird',
-    'id': 525417458249760771, # client id
+    'token': '',
+    'bot': '',
+    'id': , # client id
     'prefix': '.', # prefix e.g o.osu
-    'osu_api_key': '4d5d9874149598a5f5dec90b1e4e7990869b69b1'
+    'osu_api_key': ''
 }
