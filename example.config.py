89a09cd5db04510a367c5eea6c9c5417e9576484
divinity1437 <creonplays@gmail.com>
2022-01-08 17:50:20 +0500

    BIG THANKS TESTER

---
+++ example.config.py
@@ -0,0 +1,14 @@
+settings = {
+    'token': '',
+    'bot': '',
+    'id': '', # client id
+    'prefix': '.', # prefix e.g o.osu
+    'osu_api_key': ''
+}
+
+sql = {
+    'db': '',
+    'host': '',
+    'password': '',
+    'user': ''
+}
